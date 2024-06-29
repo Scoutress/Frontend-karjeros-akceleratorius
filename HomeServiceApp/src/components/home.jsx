d23f261e9fa33547d84d68b60a23c583636d7df5
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-06-29 16:48:08 +0300

    Created main part of home page

---
+++ HomeServiceApp/src/components/home.jsx
@@ -0,0 +1,24 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <h1>
+            Find Home Service/Repair
+            <br />
+            Near You
+          </h1>
+          <h3>Explore Best Home Service & Repair near you</h3>
+        </div>
+        <input type="text" placeholder="Search" />
+        <button>
+          <AiOutlineSearch style={{ marginRight: "5px" }} />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
