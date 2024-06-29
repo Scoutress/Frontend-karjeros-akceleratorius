722f86c2ae7afe71899e13e9b18556d06043bd0c
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-06-29 17:56:58 +0300

    Created announcement component

--- HomeServiceApp/src/components/home.jsx
+++ HomeServiceApp/src/components/home.jsx
@@ -1,25 +1,10 @@
-import { AiOutlineSearch } from "react-icons/ai";
 import HomeCategories from "../components/HomeCategories";
+import Announcement from "../components/Announcement";
 
 const Home = () => {
   return (
     <div>
-      <header>
-        <div>
-          <div>
-            <h1>
-              Find Home Service/Repair
-              <br />
-              Near You
-            </h1>
-            <h3>Explore Best Home Service & Repair near you</h3>
-          </div>
-          <input type="text" placeholder="Search" />
-          <button>
-            <AiOutlineSearch style={{ marginRight: "5px" }} />
-          </button>
-        </div>
-      </header>
+      <Announcement />
       <br></br>
       <HomeCategories />
     </div>
