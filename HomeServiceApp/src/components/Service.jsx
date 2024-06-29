06fc04b4a6841b24b8c5173140ce52c0d500b4be
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-06-29 18:13:53 +0300

    Created service component with hardcoded data

---
+++ HomeServiceApp/src/components/Service.jsx
@@ -0,0 +1,18 @@
+const Service = () => {
+  return (
+    <div>
+      <img
+        src="../../public/images/15-Cleaning-Tips-from-Professional-Cleaners-3.jpg"
+        alt="Logo"
+        style={{ height: "75px" }}
+      />
+      <h4>Cleaning</h4>
+      <p>Išsivalyk pats</p>
+      <p>Tadas Gedvilas</p>
+      <p>Kauno g. 27, Vilnius</p>
+      <button>Book now</button>
+    </div>
+  );
+};
+
+export default Service;
