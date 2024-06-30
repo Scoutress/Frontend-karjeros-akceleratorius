fbffd25cfa43598c312664d72b44c257b054c748
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-06-30 11:07:42 +0300

    Added announcement part styling

--- HomeServiceApp/src/pages/Home.jsx
+++ HomeServiceApp/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import HomeCategories from "../components/HomeCategories";
-import Announcement from "../components/Announcement";
+import Announcement from "../components/Announcement/Announcement";
 import HomePopular from "../components/HomePopular";
 
 const Home = () => {
   return (
     <div>
       <Announcement />
-      <br />
       <HomeCategories />
-      <br />
       <HomePopular />
     </div>
   );
